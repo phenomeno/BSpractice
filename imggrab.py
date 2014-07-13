c6a235e130f9a7f7beaa2638486eaa4f04064946
Grace Lee <gralee10@gmail.com>
2014-07-13 15:17:21 -0700

    Cleaned up file.

--- imggrab.py
+++ imggrab.py
@@ -28,39 +28,3 @@ f = open('imggrab.html', 'w')
 for link in u:
 	f.write('<img src="' + link + '"/>')
 	
-"""
-x = imgs[0]
-y = x.findAll(["img"])
-z= y[0]
-print z['src']
-
-
-
-for img in imgs:
-	print img.a['href'].split("imgurl=")[1]
-
-for x in soup.select('div#mw-content-text > ul > li > a'):
-	
-	print x['title']
-	
-	a = 'http://en.wikipedia.org' + x['href']
-	
-	html2 = urllib2.urlopen(a).read()
-	
-	soup2 = BeautifulSoup(html2)
-	
-	
-	x = soup2.select('#mw-content-text > ul > li > a')
-	
-	
-	co = x[0]
-	
-	print co['href']
-	
-"""
-	
-# 3. display photo url
-# 4. add titles
-	
-	
-
