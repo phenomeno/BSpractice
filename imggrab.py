7d44b3e249266967b8645ec76c374d8c9ad8ffc1
Grace Lee <gralee10@gmail.com>
2014-07-13 16:08:56 -0700

    Trouble with encoding.

--- imggrab.py
+++ imggrab.py
@@ -1,3 +1,6 @@
+#-*- coding: utf-8 -*-
+
+
 # 1. find website links
 # 2. get photo url
 
@@ -9,9 +12,14 @@ html = urllib2.urlopen('http://cp-studio.tistory.com/544').read()
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(html)
 
+#3. grab the blog title
 
+title = soup.select('div.titleWrap > h2 > a')
+title= title[0]
+title1= title.string
+title1 = title1.encode('utf-8')
 
-
+#4. grab images 
 imgs = soup.findAll('span', {'class':"imageblock"})
 
 u = []
@@ -21,10 +29,16 @@ for img in imgs:
 	z=y[0]
 	u.append(z['src'])
 	
+	
+#5. add title	
 
 f = open('imggrab.html', 'w')
+f.write('<p>' + title1 + '</p>')
 
 
+
+#6. add images
 for link in u:
-	f.write('<img src="' + link + '"/>')
+	f.write('<b><img src="' + link + '"/></b>')
 	
+f.close
